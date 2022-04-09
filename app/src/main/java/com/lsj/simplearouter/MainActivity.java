db27683756588a8208fc5bb1068b1619dfd59497
linshujie <1010368787@qq.com>
2022-04-09 12:43:07 +0800

    1.完成组件化环境部署

--- app/src/main/java/com/lsj/simplearouter/MainActivity.java
+++ app/src/main/java/com/lsj/simplearouter/MainActivity.java
@@ -2,9 +2,13 @@ package com.lsj.simplearouter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.lsj.arouter_annotations.ARouter;
+import com.lsj.module_one.ModuleFirstActivity;
+import com.lsj.module_second.ModuleSecondActivity;
 
 @ARouter(path = "/app/MainActivity",group = "app")
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gotoModuleFirst(View view) {
+        Intent intent = new Intent(this, ModuleFirstActivity.class);
+        startActivity(intent);
+    }
+
+    public void gotoModuleSecond(View view) {
+        Intent intent = new Intent(this, ModuleSecondActivity.class);
+        startActivity(intent);
     }
 }
---
+++ module-second/src/main/java/com/lsj/module_second/debug/ModuleSecondDebugActivity.java
@@ -0,0 +1,16 @@
+package com.lsj.module_second.debug;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lsj.module_second.R;
+
+public class ModuleSecondDebugActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_module_second_debug);
+    }
+}
